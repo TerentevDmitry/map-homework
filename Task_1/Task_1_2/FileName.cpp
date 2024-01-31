fdf1fd87d15df5b9bb6091386f50bdd5a8836961
TerentevDmitry <Zabey52@gmail.com>
2024-02-01 00:29:14 +0300

    v2

---
+++ Task_1/Task_1_2/FileName.cpp
@@ -0,0 +1,82 @@
+﻿//#include <iostream>
+//#include <thread>
+//#include <vector>
+//#include <chrono>
+//#include <Windows.h>
+//#include <iomanip>
+//
+//float sum = 0;
+//
+//void sumOfTwoVectors(std::vector<int> vec1, std::vector<int> vec2) 
+//{
+//	for (int i = 0; i < vec1.size(); ++i) 
+//	{
+//		sum += vec1.at(i) + vec2.at(i);
+//	}
+//}
+//int main99() 
+//{
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//	int CPU = std::thread::hardware_concurrency();
+//	std::cout << "Êîëè÷åñòâî àïïàðàòíûõ ÿäåð - " << std::thread::hardware_concurrency() << std::endl;
+//	std::vector<int> vector1;
+//	std::vector<int> vector2;
+//	std::vector<int> vectorsSizes{ 1000, 10000, 100000, 1000000 };
+//	std::vector<int> countCPU{ 1, 2, 4, 8, 16 };
+//	std::vector<int> countsCPU;
+//	std::string needSpace(12, ' ');
+//	std::cout << std::string(16, ' ');
+//	for (const auto& el : vectorsSizes) 
+//	{
+//		if (el == vectorsSizes.at(0)) 
+//		{
+//			std::cout << el << needSpace;
+//		}
+//		else if (el == vectorsSizes.at(1)) 
+//		{
+//			std::cout << el << std::string(11, ' ');
+//		}
+//		else {
+//			std::cout << el << std::string(10, ' ');
+//		}
+//
+//	}
+//	std::cout << std::endl;
+//	for (const auto& count : countCPU) 
+//	{
+//		std::cout << count << " ïîòîêîâ\t";
+//		for (const auto& vectorSize : vectorsSizes) 
+//		{
+//			vector1.resize(vectorSize, 1);
+//			vector2.resize(vectorSize, 2);
+//			std::vector<std::thread> vectorThreads;
+//			int vectorsSize = vectorSize / count;
+//			
+//			auto start = std::chrono::steady_clock::now(); // çàïîìèíàåì âðåìÿ íà÷àëà âûïîëíåíèÿ
+//			for (int i = 0; i < count; i++) 
+//			{
+//				std::vector<int> newVector1;
+//				std::vector<int> newVector2;
+//				int rightBorder = (i != count - 1) ? vectorsSize * (i + 1) : vectorSize;
+//				for (int j = vectorsSize * i; j < rightBorder; ++j) 
+//				{
+//а					newVector1.push_back(vector1.at(j));
+//					newVector2.push_back(vector2.at(j));
+//				}
+//				vectorThreads.push_back(std::thread(sumOfTwoVectors, newVector1, newVector2));
+//			}
+//			for (auto& v : vectorThreads) 
+//			{
+//				v.join();
+//			}
+//			auto stop = std::chrono::steady_clock::now(); // çàïîìèíàåì âðåìÿ îêîí÷àíèÿ âûïîëíåíèÿ
+//			auto duration = std::chrono::duration_cast<std::chrono::microseconds> (stop - start); // âû÷èñëÿåì âðåìÿ âûïîëíåíèÿ â ìèêðîñåêóíäàõ
+//			std::cout << static_cast<double>(duration.count()) / 1e6 << "s\t";
+//			//std::cout << "sum: " << sum << std::endl; // ïðîâåðêà âåðíîñòè ðàñ÷¸òà
+//			sum = 0;
+//		}
+//		std::cout << std::endl;
+//	}
+//	return 0;
+//}
--- Task_1/Task_1_2/Task_1_2.cpp
+++ Task_1/Task_1_2/Task_1_2.cpp
@@ -6,11 +6,11 @@
 #include <vector>
 #include <chrono>
 
-void sumVectors(const std::vector<int>& v1, const std::vector<int>& v2, std::vector<int>& result, int begin, int end) 
+void sumVectors(const std::vector<int>& vector1, const std::vector<int>& vector2, std::vector<int>& sumOfVectors, int begin, int end)
 {
     for (int i = begin; i < end; ++i) 
     {
-        result[i] = v1[i] + v2[i];
+        sumOfVectors[i] = vector1[i] + vector2[i];
     }
 }
 
@@ -21,7 +21,6 @@ int main()
     std::cout << "-------------------------------------------------------------------------" << std::endl;
      
     // Creating two vectors
-    //size_t sizeOfVector = 1'000;
     std::vector<int> vector1;
     std::vector<int> vector2;
 
@@ -33,45 +32,45 @@ int main()
 
     // Vector for num of threads
     std::vector<int> numOfThreads{ 1, 2, 4, 8, 16 };
-    std::vector<int> sizeOfVector{ 1'000, 10'000, 100'000, 1'000'000 };
+    std::vector<int> sizeOfVector{ 1'0, 10'000, 100'000, 1'000'000 };
 
-    for (int j = 0; j < numOfThreads[j]; ++j)
-    
+    for (auto& countT : numOfThreads)
     {
-        std::cout << std::endl << numOfThreads[j] << " threads";
-        threads.clear();
-      
-
-        for (int i = 0; i < sizeOfVector[i]; ++i)
+        if (countT == 1)
         {
-            vector1.resize(sizeOfVector[i]);
-            vector2.resize(sizeOfVector[i]);
-            sumOfVectors.resize(sizeOfVector[i]);
+            std::cout << std::endl << countT << " thread";
+        } 
+        else 
+        {
+            std::cout << std::endl << countT << " threads";
+        }
+        
+        for (auto& countV : sizeOfVector)
+        {
+            vector1.resize(countV, 2);
+            vector2.resize(countV, 5);
+            sumOfVectors.resize(countV);
 
-
-            int chunkSize = static_cast<int> (sizeOfVector[i] / numOfThreads[j]);
+            int partOfTheSize = static_cast<int> (countV / countT);
+            
             auto thr1_10000_start = std::chrono::high_resolution_clock::now();
-            threads.push_back(std::thread(sumVectors, std::ref(vector1), std::ref(vector2), std::ref(sumOfVectors), j * chunkSize, (j + 1) * chunkSize));
+
+
+            threads.push_back(std::thread(sumVectors, std::ref(vector1), std::ref(vector2), std::ref(sumOfVectors), countT * partOfTheSize, (countT + 1) * partOfTheSize));
+            
+            
             auto thr1_10000_stop = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double, std::milli> thr1_10000_time = thr1_10000_stop - thr1_10000_start;
             std::cout << std::setw(9) << "\t" << thr1_10000_time.count() << "ms";
 
 
 
-
         }
-
-    }
-
-    
-
         for (auto& it : threads)
         {
             it.join();
         }
-        
-    
-
+    }
 
     std::cout << "\n\n";
     return 0;
