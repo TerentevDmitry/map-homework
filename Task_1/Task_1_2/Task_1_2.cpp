13048edfc072b973a6ad7c0493d8851b95ce26dd
TerentevDmitry <Zabey52@gmail.com>
2024-02-01 21:52:38 +0300

    v3

--- Task_1/Task_1_2/Task_1_2.cpp
+++ Task_1/Task_1_2/Task_1_2.cpp
@@ -6,34 +6,27 @@
 #include <vector>
 #include <chrono>
 
-void sumVectors(const std::vector<int>& vector1, const std::vector<int>& vector2, std::vector<int>& sumOfVectors, int begin, int end)
+static void sumVectors(double& sumOfVectors, const std::vector<int> newVector1, const std::vector<int> newVector2)
 {
-    for (int i = begin; i < end; ++i) 
+    for (int i = 0; i < newVector1.size(); ++i)
     {
-        sumOfVectors[i] = vector1[i] + vector2[i];
+        sumOfVectors += newVector1.at(i) + newVector2.at(i);
     }
 }
 
 int main() 
 {
     std::cout << "Number of hardware cores: " << std::thread::hardware_concurrency() << "\n\n";
-    std::cout << std::setw(9) << "\t\t\t1'000" << "\t   10'000" << "\t  100'000" << "\t1'000'000" << "\n";
+    std::cout << std::setw(9) << "\t\t1'000" << "\t        10'000" << "\t       100'000" << "\t     1'000'000" << "\n";
     std::cout << "-------------------------------------------------------------------------" << std::endl;
-     
     // Creating two vectors
     std::vector<int> vector1;
     std::vector<int> vector2;
-
-    // Vector for storing the result
-    std::vector<int> sumOfVectors;
-
-    // Vector for threads
-    std::vector<std::thread> threads;
-
     // Vector for num of threads
     std::vector<int> numOfThreads{ 1, 2, 4, 8, 16 };
-    std::vector<int> sizeOfVector{ 1'0, 10'000, 100'000, 1'000'000 };
-
+    // Vector for size of Vectors
+    std::vector<int> sizeOfVector{ 1'000, 10'000, 100'000, 1'000'000 };
+   
     for (auto& countT : numOfThreads)
     {
         if (countT == 1)
@@ -45,33 +38,49 @@ int main()
             std::cout << std::endl << countT << " threads";
         }
         
-        for (auto& countV : sizeOfVector)
+       for (auto& countV : sizeOfVector)
         {
             vector1.resize(countV, 2);
             vector2.resize(countV, 5);
-            sumOfVectors.resize(countV);
+            std::vector<std::thread> threads;
+            double sumOfVectors = 0;
 
             int partOfTheSize = static_cast<int> (countV / countT);
             
-            auto thr1_10000_start = std::chrono::high_resolution_clock::now();
+            auto thrStart = std::chrono::high_resolution_clock::now();
+            for (int i = 0; i < countT; i++) 
+            {
+                std::vector<int> newVector1;
+                std::vector<int> newVector2;
+                int rightBorder = 0;
 
+                if (i != countT - 1)
+                {
+                    rightBorder = partOfTheSize * (i + 1);
+                }
+                else
+                {
+                    rightBorder = countV;
+                }
 
-            threads.push_back(std::thread(sumVectors, std::ref(vector1), std::ref(vector2), std::ref(sumOfVectors), countT * partOfTheSize, (countT + 1) * partOfTheSize));
-            
-            
-            auto thr1_10000_stop = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double, std::milli> thr1_10000_time = thr1_10000_stop - thr1_10000_start;
-            std::cout << std::setw(9) << "\t" << thr1_10000_time.count() << "ms";
+                for (int j = partOfTheSize * i; j < rightBorder; ++j)
+                {
+                    newVector1.push_back(vector1.at(j));
+                    newVector2.push_back(vector2.at(j));
+                }
+                //sumVectors(std::ref(sumOfVectors), std::ref(newVector1), std::ref(newVector2));
+                threads.push_back(std::thread(sumVectors, std::ref(sumOfVectors), newVector1, newVector2));
+            }
+            for (auto& it : threads)
+            {
+                it.join();
+            }
 
-
-
+            auto thrStop = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> thrTime = thrStop - thrStart;
+            std::cout << std::setw(14) << /*"\t" <<*/ thrTime.count() << "ms";
         }
-        for (auto& it : threads)
-        {
-            it.join();
-        }
+        std::cout << "\n\n";
     }
-
-    std::cout << "\n\n";
     return 0;
 }
