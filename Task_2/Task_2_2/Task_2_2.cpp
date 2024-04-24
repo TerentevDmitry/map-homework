b48058f09c75c0b5a49fffc32ad41df48db27378
TerentevDmitry <Zabey52@gmail.com>
2024-04-24 20:03:01 +0300

    zad2_3_v4

--- Task_2/Task_2_2/Task_2_2.cpp
+++ Task_2/Task_2_2/Task_2_2.cpp
@@ -18,12 +18,12 @@ std::mutex m4;
 void tableHeader() { std::cout << "#\t" << "id\t" << "Progress Bar\t" << "Time" << std::endl; }
 
 
-static void progressBar(const int numberThreadsNow)
+static void progressBar(int numberThreadsNow)
 {
+    
     int progress = 15;
     int total = 25;
     int PositionForTime = 35;
-
     
     std::call_once(flag, tableHeader);    
     
@@ -35,15 +35,16 @@ static void progressBar(const int numberThreadsNow)
 
     while (progress < total)
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        m1.lock();
         
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        
+        m1.lock();
         d1.SetPosition(progress, numberThreadsNow + 1);
 
-        //std::cout << char(178);
+        std::cout << char(178);
         //std::cout << '\b';
         
-        std::cout << char(219);
+        //std::cout << char(219);
         
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         
@@ -51,11 +52,16 @@ static void progressBar(const int numberThreadsNow)
         ++progress;
         m1.unlock();
     }
+    std::this_thread::sleep_for(std::chrono::milliseconds(333));
     
     m3.lock();
-    d1.SetPosition(PositionForTime, numberThreadsNow + 1);
+    d1.SetPosition(35, numberThreadsNow + 1);
     t1.print();
+    
     m3.unlock();
+  
+    
+    //std::cout << "\n\n\n\n";
 }
 
 int main()
@@ -70,6 +76,7 @@ int main()
     for (auto& i : vectorThreads)
     {
         i.join();
+        
     }
-    std::cout <<  '\n\n\n\n';
+    std::cout << "\n\n\n\nENENE";
 }
