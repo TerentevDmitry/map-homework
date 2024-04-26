7478321c754227e25c4dc17bcf4dffadfaa8d192
TerentevDmitry <Zabey52@gmail.com>
2024-04-26 18:54:57 +0300

    zad2_2

--- Task_2/Task_2_2/Task_2_2.cpp
+++ Task_2/Task_2_2/Task_2_2.cpp
@@ -15,58 +15,50 @@ std::mutex m2;
 std::mutex m3;
 std::mutex m4;
 
-static void tableHeader() { std::cout << "#\t" << "id\t" << "Progress Bar\t" << "Time" << std::endl; }
-
+static void tableHeader() { std::cout << "#\t" << "id-thread\t" << "Progress Bar\t\t" << "Time" << std::endl; }
 
 static void progressBar(int numberThreadsNow)
 {
+    int progressBar = 23;
+    int total = 37;
+    int PositionForTime = 43;
     
-    int progress = 15;
-    int total = 25;
-    int PositionForTime = 35;
-    
-    std::call_once(flag, tableHeader);    
-    
-
+    std::call_once(flag, tableHeader);
+        
     m2.lock();
     Timer t1;
     consol_parameter d1;
     m2.unlock();
 
-    while (progress < total)
+    m4.lock();
+    d1.SetPosition(0, numberThreadsNow + 1);
+    std::cout << numberThreadsNow << "\t" << std::this_thread::get_id() << "\t";
+    m4.unlock();
+
+    while (progressBar < total)
     {
-        
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         
         m1.lock();
-        d1.SetPosition(progress, numberThreadsNow + 1);
-
-        std::cout << char(178);
-        //std::cout << '\b';
-        
-        //std::cout << char(219);
+        d1.SetPosition(progressBar, numberThreadsNow + 1);
+        std::cout << char(219);
         
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         
-        
-        ++progress;
+        ++progressBar;
         m1.unlock();
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(333));
     
     m3.lock();
-    d1.SetPosition(35, numberThreadsNow + 1);
+    d1.SetPosition(PositionForTime, numberThreadsNow + 1);
     t1.print();
-    
     m3.unlock();
-  
-    
-    //std::cout << "\n\n\n\n";
 }
 
 int main()
 {
-    int countThreads = 3;
+    int countThreads = 5;
     std::vector<std::thread> vectorThreads(countThreads);
     
     for (size_t i = 0; i < countThreads; i++)
@@ -76,7 +68,6 @@ int main()
     for (auto& i : vectorThreads)
     {
         i.join();
-        
     }
     std::cout << "\n\n\n\nENENE";
 }
