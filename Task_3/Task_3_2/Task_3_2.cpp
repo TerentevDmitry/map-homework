1b58a7c1cb3bd7046d61d40a3276abd1a6e7313b
TerentevDmitry <Zabey52@gmail.com>
2024-05-06 22:29:57 +0300

    3_2_v2

--- Task_3/Task_3_2/Task_3_2.cpp
+++ Task_3/Task_3_2/Task_3_2.cpp
@@ -3,24 +3,34 @@
 #include <algorithm>
 #include <future>
 
-template <typename Iterator, typename Function>
-void parallelForEach(Iterator first, Iterator last, Function f)
+void multiplyVec(int& MultiplierVec)
 {
-    std::vector<std::future<void>> futures;
-    for (Iterator it = first; it != last; ++it)
-    {
-        futures.push_back(std::async(std::launch::async, f, std::ref(*it)));
-    }
+    MultiplierVec *= 2;
+}
 
-    for (std::future<void>& future : futures)
+template <typename Iterator, size_t chunkSize = 3>
+void parallelForEachAsyncRecursive(Iterator begin, Iterator end)
+{
+    auto currSize = std::distance(begin, end);
+    
+    if (currSize <= chunkSize)
     {
-        future.get();
+        std::for_each(begin, end, multiplyVec);
+    }
+    else
+    {
+        auto mid = begin;
+        std::advance(mid, currSize / 2);
+        auto ftRes = std::async(parallelForEachAsyncRecursive<Iterator>, begin, mid);
+        parallelForEachAsyncRecursive(mid, end);
+
+        ftRes.get();
     }
 }
 
 int main()
 {
-    std::vector<int> vec = { 1, 2, 3, 4, 5 };
+    std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
     std::cout << "Before: ";
     for (int i : vec)
@@ -29,12 +39,9 @@ int main()
     }
     std::cout << std::endl;
 
-    parallelForEach(vec.begin(), vec.end(), [](int& i)
-        {
-            i *= 2;
-        });
+    parallelForEachAsyncRecursive(vec.begin(), vec.end());
 
-    std::cout << "After: ";
+    std::cout << "\nAfter: ";
     for (int i : vec)
     {
         std::cout << i << " ";
