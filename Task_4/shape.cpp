effb2a10f1705741eac585822f5ebe262093bd48
TerentevDmitry <Zabey52@gmail.com>
2024-05-10 09:35:19 +0300

    функция создания Shape shLine,shSqr,shCube - без switch

--- Task_4/shape.cpp
+++ Task_4/shape.cpp
@@ -6,102 +6,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-Shape::Shape(int _type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8)
-{
-	type = _type;
-	// заполн¤ем координаты фигуры
-	switch (type)
-	{
-	case line: 
-		x1 = _x1; y1 = _y1;
-		x2 = _x2; y2 = _y2;
-		break;
-	case sqr:
-		x1 = _x1; y1 = _y1;
-		x2 = _x2; y2 = _y2;
-		x3 = _x3; y3 = _y3;
-		x4 = _x4; y4 = _y4;
-		break;
-	case cube:
-		x1 = _x1; y1 = _y1; z1 = _z1;
-		x2 = _x2; y2 = _y2; z2 = _z2;
-		x3 = _x3; y3 = _y3; z3 = _z3;
-		x4 = _x4; y4 = _y4; z4 = _z4;
-		x5 = _x5; y5 = _y5; z5 = _z5;
-		x6 = _x6; y6 = _y6; z6 = _z6; 
-		x7 = _x7; y7 = _y7; z7 = _z7;
-		x8 = _x8; y8 = _y8; z8 = _z8;
-		break;
-	default:
-		break;
-	}
-	
-
-	int a = abs(x1 - x2);
-	int b = abs(y1 - y2);
-	int c = abs(z1 - z2);
-	
-	//double distanceBetweenOppositeVertices = sqrt(pow(x1 - x8, 2) + pow(y1 - y8, 2) + pow(z1 - z8, 2));
-	
-	// считаем площадь фигуры
-	
-	switch (type)
-	{
-	case line:
-		square = 0;
-		break;
-	case sqr:
-		square = pow((x1 - x2), 2) + pow((y1 - y2), 2);
-		break;
-	case cube:
-		square = 6 * (pow((x1 - x2), 2) + pow((y1 - y2), 2));
-		break;
-	default:
-		break;
-	}
-	
-
-	// считаем объем фигуры
-	switch (type)
-	{
-	case line:
-		volume = 0;
-		break;
-	case sqr:
-		volume = 0;
-		break;
-	case cube:
-		volume = 2 * (pow((x1 - x2), 2) + pow((y1 - y2), 2));
-		break;
-	default:
-		break;
-	}
-
-	
-	
-	
-	
-		
-	
-
-
-
-
-
-
-
-}
-
 Shape::Shape(int _type, int _x1, int _y1, double R, double H)
 {
 	type = _type;
@@ -149,6 +53,51 @@ Shape::Shape(int _type, int _x1, int _y1, double R, double H)
 	}
 
 }
+
+
+
+Shape::Shape(int _x1, int _y1, int _x2, int _y2)
+{
+	type = static_cast<int> (nameOfShapes::line);
+	x1 = _x1; y1 = _y1;
+	x2 = _x2; y2 = _y2;
+	square = 0.;
+	volume = 0.;
+}
+
+Shape::Shape(int _x1, int _y1, int _x2, int _y2, int _x3, int _y3, int _x4, int _y4)
+{
+	type = static_cast<int> (nameOfShapes::sqr);
+	x1 = _x1; y1 = _y1;
+	x2 = _x2; y2 = _y2;
+	x3 = _x3; y3 = _y3;
+	x4 = _x4; y4 = _y4;
+	square = pow((x1 - x2), 2) + pow((y1 - y2), 2);
+	volume = 0.;
+}
+
+Shape::Shape(int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8)
+{
+	type = static_cast<int> (nameOfShapes::cube);
+	x1 = _x1; y1 = _y1; z1 = _z1;
+	x2 = _x2; y2 = _y2; z2 = _z2;
+	x3 = _x3; y3 = _y3; z3 = _z3;
+	x4 = _x4; y4 = _y4; z4 = _z4;
+	x5 = _x5; y5 = _y5; z5 = _z5;
+	x6 = _x6; y6 = _y6; z6 = _z6;
+	x7 = _x7; y7 = _y7; z7 = _z7;
+	x8 = _x8; y8 = _y8; z8 = _z8;
+	square = 6 * (pow((x1 - x2), 2) + pow((y1 - y2), 2));
+	volume = 2 * (pow((x1 - x2), 2) + pow((y1 - y2), 2));
+}
+
+
+
+
+
+
+
+
 
 int Shape::getType() const
 {
